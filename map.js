c55affc2c22e64fa72818cc3f114ee7001d7033d
Reina Chano Murray <r.chano@gmail.com>
2016-11-11 13:42:50 -0500

    Update map.js

--- map.js
+++ map.js
@@ -28,7 +28,7 @@ var reurbIcon_grey = L.icon({
 
 
 //Load basemap
-var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+var Stamen_TonerLite = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 	subdomains: 'abcd',
 	minZoom: 0,
